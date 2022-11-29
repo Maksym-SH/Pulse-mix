fe6c8ea73b283ccb362e368b33b108d58ae26c1e
Maksym-SH <maxim2002sherba4yk@gmail.com>
2022-11-29 23:02:22 +0300

    Project init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <notifications/>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+#app {
+  min-width: $minWidth;
+  font-family: $NSRegular;
+  background-color: $color-light-grey;
+}
+</style>
