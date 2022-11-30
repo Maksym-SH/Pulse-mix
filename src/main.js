795a9317899333237a1e263f4080f9b8348c1575
Maksym-SH <maxim2002sherba4yk@gmail.com>
2022-11-30 23:33:11 +0300

    Added params to starting project

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Notifications from "vue-notification";
+import SsrCarousel from 'vue-ssr-carousel';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-ssr-carousel/index.css';
+
+Vue.component('ssr-carousel', SsrCarousel);
 Vue.use(Notifications);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
@@ -12,5 +21,5 @@ Vue.config.devtools = true;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount("#app");
